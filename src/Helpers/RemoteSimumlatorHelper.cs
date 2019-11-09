de465c5c7bfd95bbae11426ee8fab46b77a97f83
Evolutionlab <info@evolutionlab.it>
2019-11-09 12:53:07 +0100

    add files missing from previous commit

---
+++ src/Helpers/RemoteSimumlatorHelper.cs
@@ -0,0 +1,32 @@
+﻿using RemoteSimulator.Core;
+
+namespace RemoteSimulator.Helpers
+{
+    public static class RemoteSimumlatorHelper
+    {
+        public static void OpenSimulator(string uid)
+        {
+            string strCmdText = $"--device={uid} -h={Config.MacHost.Host} -ssh={Config.MacHost.User}";
+
+            var process = new System.Diagnostics.Process
+            {
+                StartInfo = new System.Diagnostics.ProcessStartInfo
+                {
+                    UseShellExecute        = false,
+                    RedirectStandardOutput = true,
+                    CreateNoWindow         = true,
+
+                    WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden,
+                    FileName    = Config.RemoteSimulatorPath,
+                    Arguments   = strCmdText
+                }
+            };
+            process.Start();
+
+#if DEBUG
+            process.WaitForExit();
+#endif
+
+        }
+    }
+}
